f1d1b920f9242e3a7e0197ff681d558fc25c583a
Usuario <Usuario@Usuario-PC>
2014-02-11 18:45:40 -0200

    ESTO ES UNA PRUEBA

--- src/Fachada/Servicios.java
+++ src/Fachada/Servicios.java
@@ -10,8 +10,9 @@ import prototipo.Utilitario.Utilitario;
  *
  * @author Deibys
  */
+// prueba de commit
 public class Servicios {
-
+    
     public boolean procesaEgresoManual(String parametro) {
         Utilitario.mensajeError("Falta implementar", "Metodos en Servicios");
         return false;
